33b977eb29abf99f882718ab5f21a1f871328ce5
Bob Barthel <bob.barthel@outlook.de>
2025-07-03 13:24:00 +0200

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte(), tailwindcss()],
-  base: '/BOLE-WEBSITE/', // Add this to match GitHub Pages URL
+  base: '/', // Add this to match GitHub Pages URL
 });
